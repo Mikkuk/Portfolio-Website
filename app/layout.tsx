dd8b8fb8dbc5600180284f37b2ebb4cce2e5af1c
Mikkuk <mikko.kukkonenhml@gmail.com>
2024-08-12 15:32:41 +0300

    fix tailwind congfig

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31rem]
+         w-[31rem] rounded-full blur-[10rem] sm:w-[69rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31rem]
+         w-[50rem] rounded-full blur-[10rem] sm:w-[69rem] md:left-[-33rem] lg:left-[-28rem]
+          xl:left-[-15rem]"></div>
+        {children}
+      </body>
     </html>
   );
 }
