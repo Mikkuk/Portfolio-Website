509c1ecdfcbbdcc7b06243e8bd69466b951c4c7b
Mikkuk <mikko.kukkonenhml@gmail.com>
2024-08-12 17:02:12 +0300

    add header section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "../components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31rem]
-         w-[31rem] rounded-full blur-[10rem] sm:w-[69rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31rem]
-         w-[50rem] rounded-full blur-[10rem] sm:w-[69rem] md:left-[-33rem] lg:left-[-28rem]
+        <div className="bg-[#ebcaf3] absolute top-[-6rem] -z-10 right-[11rem] h-[31rem]
+         w-[31rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem]
+         w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
           xl:left-[-15rem]"></div>
+
+        <Header/>
         {children}
       </body>
     </html>
