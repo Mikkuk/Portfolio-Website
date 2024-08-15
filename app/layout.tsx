79ff5399a91f9317dcabcd53e55933ab9ad990f6
Mikkuk <mikko.kukkonenhml@gmail.com>
2024-08-15 16:50:49 +0300

    add active section context provider to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:pt-36`}>
         <div className="bg-[#ebcaf3] absolute top-[-6rem] -z-10 right-[11rem] h-[31rem]
          w-[31rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
@@ -24,8 +25,10 @@ export default function RootLayout({
          w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
           xl:left-[-15rem]"></div>
 
-        <Header/>
-        {children}
+        <ActiveSectionContextProvider>
+          <Header/>
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
