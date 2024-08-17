5445cdc056ec683363bc23c0aa836e84564a9349
Mikkuk <mikko.kukkonenhml@gmail.com>
2024-08-17 17:40:02 +0300

    add theme context provider to component tree

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,52 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "../components/header";
-import ActiveSectionContextProvider from "@/context/active-section-context";
-import Footer from "@/components/footer";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '../components/header';
+import ActiveSectionContextProvider from '@/context/active-section-context';
+import ThemeSwitch from '@/components/theme-switch';
+import ThemeContextProvider from '@/context/theme-context';
+import Footer from '@/components/footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Mikko Kukkonen | Portfolio",
-  description: "Portfolio website for Mikko Kukkonen",
+    title: 'Mikko Kukkonen | Portfolio',
+    description: 'Portfolio website for Mikko Kukkonen',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:pt-36`}>
-        <div className="bg-[#ebcaf3] absolute top-[-6rem] -z-10 right-[11rem] h-[31rem]
-         w-[31rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-2rem] -z-10 left-[-35rem] h-[31.25rem]
-         w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
-          xl:left-[-15rem]"></div>
+    return (
+        <html
+            lang="en"
+            className="!scroll-smooth"
+        >
+            <body
+                className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+            >
+                <div
+                    className="bg-[#ebcaf3] absolute top-[-6rem] -z-10 right-[11rem] h-[31rem]
+                    w-[31rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"
+                ></div>
+                <div
+                    className="bg-[#dbd7fb] absolute top-[-2rem] -z-10 left-[-35rem] h-[31.25rem]
+                  w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
+                  xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"
+                ></div>
 
-        <ActiveSectionContextProvider>
-          <Header/>
-          {children}
-          <Footer/>
-        </ActiveSectionContextProvider>
-      </body>
-    </html>
-  );
+                <ThemeContextProvider>
+                    <ActiveSectionContextProvider>
+                        <Header />
+                        {children}
+                        <Footer />
+
+                        <ThemeSwitch />
+                    </ActiveSectionContextProvider>
+                </ThemeContextProvider>
+            </body>
+        </html>
+    );
 }
